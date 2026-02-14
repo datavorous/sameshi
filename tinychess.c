4d26ae8c9076769906352a832c5c37dfa2eb26c0
datavorous <datavorous.work@gmail.com>
2026-02-14 08:08:12 +0530

    added forward 2, and reduced func defs

--- tinychess.c
+++ tinychess.c
@@ -25,13 +25,6 @@ int N[]={-21,-19,-12,-8,8,12,19,21}, K[]={-1,1,-10,10,-11,-9,9,11};
 
 int bs, bd;
 
-int e() {
-    int s = 0;
-    for(int i = 21; i <= 98; i++)
-        if (b[i] != 7) s += (b[i] > 0 ? v[b[i]] : -v[-b[i]]);
-    return s;
-}
-
 int n(int s, int d) {
     if (d==0) return s * e();
 
@@ -48,17 +41,29 @@ int n(int s, int d) {
 
 int S(int s, int d, int a, int be) {
     if (d==0) {
-        return s * e();
+        int sc = 0;
+        for(int i = 21; i <= 98; i++)
+            if (b[i] != 7) sc += (b[i] > 0 ? v[b[i]] : -v[-b[i]]);
+        return sc * e();
     }
+
+    int oa = a;
+    int bm = 0;
+
+
     for(int src=21; src<=98; src++) {
         int p = b[src];
         if (p == 7 || p==0 || (p>0 && s<0) || (p<0 && s>0)) continue;
 
+        int *vec = (abs(p) == 2)?N:K;
+        int len = 8;
+        if (abs(p) == 1) len = 0;
+
         if(abs(p) == 1) {
             int dir = (s == 1)? 10:-10;
             
-            int t = src + dir;
-            if (b[t] == 0) {
+            if (b[src + dir] == 0) {
+                int t = src + dir;
                 b[t] = p;
                 b[src] = 0;
 
@@ -66,11 +71,17 @@ int S(int s, int d, int a, int be) {
                 b[src] = p;
                 b[t] = 0;
 
-                if (score > a) a = score;
+                if (score > a) {a = score;
+                    if (d==4) {
+                        bs = src;
+                        bd = t;
+                    }}
                 if (a >= be) return be;
+
+
             }
             for(int i=-1; i<=1; i+= 2) {
-                if (   b[src+i+dir]!=0 &&  b[src+i+dir] != 7 && (b[src+dir+i]>0) != (s>0)) {
+                if (b[src+i+dir]!=0 && b[src+i+dir] != 7 && (b[src+dir+i]>0) != (s>0)) {
                     int t = src + dir + i;
                     int capt = b[t];
                     b[t] = p; 
@@ -80,7 +91,11 @@ int S(int s, int d, int a, int be) {
                     b[src] = p;
                     b[t] = capt;
 
-                    if (score > a) a = score;
+                    if (score > a) {a = score;
+                        if (d==4) {
+                            bs = src;
+                            bd = t;
+                        }}
                     if (a >= be) return be;
                 }
             }
